f1b1ca9090206cfccc350a4a2a0580d786b2d69a
John Stockton <johnstocktoniv@gmail.com>
2024-02-10 19:52:38 -0500

    update install-software.bat script

--- install-software.bat
+++ install-software.bat
@@ -1,17 +1,7 @@
-# Check if chocolately is installed
 choco -? || (
     echo "Chocolatey is not installed. Please install it from https://chocolatey.org/"
     exit
 )
 
-# Install programming languages
-choco install python -y
-choco install openjdk -y
-choco install nodejs -y
-
-# Install applications
-choco install vscode -y
-choco install podman-desktop -y
-choco install docker-desktop -y
-choco install steam -y
-choco install wsl2 -y
+choco install python openjdk nodejs dotnet rust go scala sqlite -y
+choco install vscode googlechrome googledrive podman-desktop docker-desktop etcher wsl2 spotify steam -y
